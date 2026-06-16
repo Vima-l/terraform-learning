2fab0a406a0693f15fcad2201ad712c8a80a3dce
Vima-l <vimalrrj07@gmail.com>
2026-06-16 07:53:05 +0530

    update api gateway authorization

--- iac/module-wise-iac/modules/apigateway/main.tf
+++ iac/module-wise-iac/modules/apigateway/main.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_resource" "hello" {
 }
 
 resource "aws_api_gateway_method" "get_hello" {
-  authorization = "None"
+  authorization = "NONE"
   http_method   = "GET"
   resource_id   = aws_api_gateway_resource.hello.id
   rest_api_id   = aws_api_gateway_rest_api.apigateway.id
